439f7827fe0c135a481839966eb171571ac1001b
iPowers <isika.powers@gmail.com>
2021-09-27 10:30:17 -0600

    Create and migrate seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+@grey = Hospital.create!(name: "Grey Sloan Memorial Hospital")
 
-hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
-hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+@meredith = @grey.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Standford University")
+@miranda = @grey.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Standford University")
+
+@denny = @meredith.patients.create!(name: "Denny Duquette", age: 39)
+@logan = @meredith.patients.create!(name: "Logan Mcgin", age: 15)
+
+@betty = @miranda.patients.create!(name: "Betty Boo", age: 71)
